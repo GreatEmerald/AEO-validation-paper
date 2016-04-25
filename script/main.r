3e02412acbeeae264634d2478d9661a760b2cc33
Dainius <pastas4@gmail.com>
2016-04-25 16:03:03 +0200

    The holdout validation is now a generic function, added MC and australia

--- script/main.r
+++ script/main.r
@@ -2,9 +2,9 @@
 install.packages("inspectr", repos="http://R-Forge.R-project.org")
 library(pls)
 library(inspectr)
-source("optisim.r")
 
 setwd("/home/dainius/Documents/Advanced Earth Observation/AEO-validation-paper/script/")
+source("optisim.r")
 
 # Load data (assemble from 2 CSVs)
 DATA1 = read.csv("soilNL_test.csv", header=TRUE);
@@ -22,6 +22,10 @@ DATA = DATA[order(DATA$Sample, DATA$RowID),]
 # Make into an inspectr SpectraDataFrame
 sDATA = DATA
 spectra(sDATA) = id ~ pH + Moisture + OM ~ 420:2500
+
+# Harmonise the names in the australia dataset
+AUSTRALIA = australia
+names(AUSTRALIA)[names(AUSTRALIA)=="ph"]="pH"
 
 # Plot validation plots
 # Wrapper function for simpler plsr
@@ -115,46 +119,66 @@ legend("topright", c("Leave-one-out", "Random", "Consecutive", "Interleaved", "M
 compnr = 16
 # Graph of training set size vs RMSE 
 
-# Random sampling (without replacement)
-sampleresponse = data.frame()
-for(i in floor((seq(4.5, 16, 0.5))^2))
+# Semi-generic function for running all the different sampling strategies
+# Returns a data.frame with relevant statistics (RMSE, R2, etc.) for the amount of samples
+# type defines which validation strategy to use: random, bootstrap, none (if samples are provided)
+# mc.runs defines how many times to run the Monte Carlo analysis (1 means no MC)
+# validation.set overrides the validation set used (when unset, non-selected samples are used)
+hvalidation = function(type = "none", samples = c(), mc.runs=1, data=DATA, validation.set=NULL, ncomp=compnr, static.seed=TRUE)
 {
-    samp = sample(nrow(DATA), i)
-    tset = DATA[samp,]
-    vset = DATA[-samp,]
-    vspec = vset[7:ncol(vset)]
-    rsm = fplsr(yvar, data=tset, ncomp=compnr, validation = "none")
-    pred.tst <- predict(rsm, comps = 1:compnr, newdata = as.matrix(vspec))
-    RMSE.xval <- sqrt(mean((vset$pH - pred.tst)^2))
-    sampleresponse = rbind(sampleresponse, data.frame(i=i, RMSE=RMSE.xval))
-    #plot (pred.tst ~ vset$pH, xlab="Observed (validation)", ylab="Predicted (training)", main="pH observed vs predicted")
-    #abline(0, 1)
+    if(static.seed)
+        set.seed(430001)
+    sampleresponse = data.frame()
+    for(i in floor((seq(4.5, 16, 0.5))^2))
+    {
+        rundata = data.frame()
+        for(run in 1:mc.runs)
+        {
+            if(type == "random")
+                samp = sample(nrow(data), i)
+            if(type == "bootstrap")
+                samp = sample(nrow(data), i, replace=TRUE)
+            if(type == "none" && is.numeric(samples))
+                samp = samples[1:i]
+            tset = data[samp,]
+            if(is.data.frame(validation.set))
+                vset = validation.set
+            else
+                vset = data[-samp,]
+            vspec = subset(vset, select=c(X420:X1849, X1951:X2500))
+            model = fplsr(yvar, data=tset, ncomp=ncomp, validation = "none")
+            pred = predict(model, comps = 1:compnr, newdata = as.matrix(vspec))
+            pred.RMSE = sqrt(mean((vset$pH - pred)^2))
+            pred.RPD = sd(vset$pH)/pred.RMSE
+            rundata = rbind(rundata, data.frame(samples=i, RMSE=pred.RMSE, RPD=pred.RPD))
+        }
+        sampleresponse = rbind(sampleresponse, colMeans(rundata))
+        names(sampleresponse) = names(rundata)
+    }
+    return(sampleresponse)
 }
-plot(sampleresponse$RMSE~sampleresponse$i, type="l")
+
+# Random sampling (without replacement)
+hv.rand = hvalidation(type="random")
+plot(hv.rand$RMSE~hv.rand$sample, type="l")
+
+# Monte Carlo equivalent
+hv.randmc = hvalidation(type="random", mc.runs=100)
+plot(hv.randmc$RMSE~hv.randmc$sample, type="l")
 
 # Random sampling (with replacement) AKA bootstrapping:
 # For example, if we have 20 samples, we pretend to have more (30) by duplicating some.
 # The validation set then uses all of the samples we didn't use for training for best estimates.
 # X values are "effective sample size", which is an inflated version of the real one.
 # Pretty much requires Monte Carlo treatment!
-sampleresponse = data.frame()
-for(i in floor((seq(4.5, 16, 0.5))^2))
-{
-    samp = sample(nrow(DATA), i, replace=TRUE)
-    tset = DATA[samp,]
-    vset = DATA[-samp,]
-    vspec = vset[7:ncol(vset)]
-    rsm = fplsr(yvar, data=tset, ncomp=compnr, validation = "none")
-    pred.tst <- predict(rsm, comps = 1:compnr, newdata = as.matrix(vspec))
-    RMSE.xval <- sqrt(mean((vset$pH - pred.tst)^2))
-    sampleresponse = rbind(sampleresponse, data.frame(i=i, RMSE=RMSE.xval))
-    #plot (pred.tst ~ vset$pH, xlab="Observed (validation)", ylab="Predicted (training)", main="pH observed vs predicted")
-    #abline(0, 1)
-}
-plot(sampleresponse$RMSE~sampleresponse$i, type="l")
+hv.boot = hvalidation(type="bootstrap")
+plot(hv.boot$RMSE~hv.boot$sample, type="l")
+
+# Monte Carlo equivalent
+hv.bootmc = hvalidation(type="bootstrap", mc.runs=100)
+plot(hv.bootmc$RMSE~hv.bootmc$sample, type="l")
 
 # Stratified sampling: can we tell soil types for non-parametric sampling?
-
 hist(DATA$pH, freq=FALSE, breaks=16)
 # Else, parametric optimisation can be applied (like kenstone, but bins instead of samples)
 # This can use k-means clustering to determine strata, but how many? 2 (peaks in pH)? 16 (components)? By pH, or other variables?
@@ -165,42 +189,118 @@ hist(DATA$pH, freq=FALSE, breaks=16)
 # Kennard-Stone algorithm: select the spectra with the largest differences
 # Rather slow! It returns the same things every time, so just running it
 # with the highest setting once is enough.
-sampleresponse = data.frame()
-sampfull = kenstone(sDATA, floor(16^2))
-for(i in floor((seq(4.5, 16, 0.5))^2))
-{
-    samp = sampfull[1:i]
-    tset = DATA[samp,]
-    vset = DATA[-samp,]
-    vspec = vset[7:ncol(vset)]
-    rsm = fplsr(yvar, data=tset, ncomp=compnr, validation = "none")
-    pred.tst <- predict(rsm, comps = 1:compnr, newdata = as.matrix(vspec))
-    RMSE.xval <- sqrt(mean((vset$pH - pred.tst)^2))
-    sampleresponse = rbind(sampleresponse, data.frame(i=i, RMSE=RMSE.xval))
-    #plot (pred.tst ~ vset$pH, xlab="Observed (validation)", ylab="Predicted (training)", main="pH observed vs predicted")
-    #abline(0, 1)
-}
-plot(sampleresponse$RMSE~sampleresponse$i, type="l", xlab="Samples in training set", ylab="RMSE")
+ks.full = kenstone(sDATA, 16^2)
+hv.kstn = hvalidation(type="none", samples=ks.full)
+plot(hv.kstn$RMSE~hv.kstn$sample, type="l")
 
 # OptiSim: a quicker version of Kennard-Stone, using k=4 as per paper
 # This is random subsets of 4 samples, so technically Monte Carlo would apply,
 # but the results shouldn't vary enough for it to be worth the time
 # (and definitely not if it exceeds Kennard-Stone time of calculation)
-sampleresponse = data.frame()
-sampfull = optisim(sDATA, 16^2, 4)
-for(i in floor((seq(4.5, 16, 0.5))^2))
-{
-    # Rather odd way of converting factors to IDs, since optisim() returns selected SpectraDataFrame,
-    # whereas we need the non-selected ones too
-    samp = as.numeric(levels(sampfull@id[,1]))[sampfull@id[,1]][1:i]
-    tset = DATA[samp,]
-    vset = DATA[-samp,]
-    vspec = vset[7:ncol(vset)]
-    rsm = fplsr(yvar, data=tset, ncomp=compnr, validation = "none")
-    pred.tst <- predict(rsm, comps = 1:compnr, newdata = as.matrix(vspec))
-    RMSE.xval <- sqrt(mean((vset$pH - pred.tst)^2))
-    sampleresponse = rbind(sampleresponse, data.frame(i=i, RMSE=RMSE.xval))
-    #plot (pred.tst ~ vset$pH, xlab="Observed (validation)", ylab="Predicted (training)", main="pH observed vs predicted")
-    #abline(0, 1)
-}
-plot(sampleresponse$RMSE~sampleresponse$i, type="l", xlab="Samples in training set", ylab="RMSE")
+os.full = optisim(sDATA, 16^2, 4)
+# Factors to numbers; ought to be just as.numeric, but oh well...
+os.samples = as.numeric(levels(os.full@id[,1]))[os.full@id[,1]]
+hv.osim = hvalidation(type="none", samples=os.samples)
+plot(hv.osim$RMSE~hv.osim$sample, type="l")
+
+# One large plot
+plot(hv.rand$RMSE~hv.rand$sample, type="l", xlab="Samples in training set", ylab="RMSE", col=1,
+    ylim=c(min(min(hv.rand$RMSE), min(hv.boot$RMSE), min(hv.kstn$RMSE), min(hv.osim$RMSE)),
+        max(max(hv.rand$RMSE), max(hv.boot$RMSE), max(hv.kstn$RMSE), max(hv.osim$RMSE))))
+lines(hv.randmc$RMSE~hv.randmc$sample, col=2)
+lines(hv.boot$RMSE~hv.boot$sample, col=3)
+lines(hv.bootmc$RMSE~hv.bootmc$sample, col=4)
+lines(hv.kstn$RMSE~hv.kstn$sample, col=5)
+lines(hv.osim$RMSE~hv.osim$sample, col=6)
+
+# RPD
+plot(hv.rand$RPD~hv.rand$sample, type="l", xlab="Samples in training set", ylab="RPD", col=1,
+    ylim=c(min(min(hv.rand$RPD), min(hv.boot$RPD), min(hv.kstn$RPD), min(hv.osim$RPD)),
+        max(max(hv.rand$RPD), max(hv.boot$RPD), max(hv.kstn$RPD), max(hv.osim$RPD))))
+lines(hv.randmc$RPD~hv.randmc$sample, col=2)
+lines(hv.boot$RPD~hv.boot$sample, col=3)
+lines(hv.bootmc$RPD~hv.bootmc$sample, col=4)
+lines(hv.kstn$RPD~hv.kstn$sample, col=5)
+lines(hv.osim$RPD~hv.osim$sample, col=6)
+
+## Rerun with australia as validation
+av.rand = hvalidation(type="random", validation.set=AUSTRALIA)
+av.randmc = hvalidation(type="random", mc.runs=100, validation.set=AUSTRALIA)
+av.boot = hvalidation(type="bootstrap", validation.set=AUSTRALIA)
+av.bootmc = hvalidation(type="bootstrap", mc.runs=100, validation.set=AUSTRALIA)
+# ks.full has already been calculated, no need to redo that
+av.kstn = hvalidation(type="none", samples=ks.full, validation.set=AUSTRALIA)
+# No need to recalculate os.samples either
+av.osim = hvalidation(type="none", samples=os.samples, validation.set=AUSTRALIA)
+
+plot(av.rand$RMSE~av.rand$sample, type="l", xlab="Samples in training set", ylab="RMSE", col=1,
+    ylim=c(min(min(av.rand$RMSE), min(av.boot$RMSE), min(av.kstn$RMSE), min(av.osim$RMSE)),
+        max(max(av.rand$RMSE), max(av.boot$RMSE), max(av.kstn$RMSE), max(av.osim$RMSE))))
+lines(av.randmc$RMSE~av.randmc$sample, col=2)
+lines(av.boot$RMSE~av.boot$sample, col=3)
+lines(av.bootmc$RMSE~av.bootmc$sample, col=4)
+lines(av.kstn$RMSE~av.kstn$sample, col=5)
+lines(av.osim$RMSE~av.osim$sample, col=6)
+
+plot(av.rand$RPD~hv.rand$sample, type="l", xlab="Samples in training set", ylab="RPD", col=1,
+    ylim=c(min(min(av.rand$RPD), min(av.boot$RPD), min(av.kstn$RPD), min(av.osim$RPD)),
+        max(max(av.rand$RPD), max(av.boot$RPD), max(av.kstn$RPD), max(av.osim$RPD))))
+lines(av.randmc$RPD~av.randmc$sample, col=2)
+lines(av.boot$RPD~av.boot$sample, col=3)
+lines(av.bootmc$RPD~av.bootmc$sample, col=4)
+lines(av.kstn$RPD~av.kstn$sample, col=5)
+lines(av.osim$RPD~av.osim$sample, col=6)
+
+## Retry everything with 6 components
+hv6.rand = hvalidation(type="random", ncomp=12)
+hv6.randmc = hvalidation(type="random", mc.runs=100, ncomp=6)
+hv6.boot = hvalidation(type="bootstrap", ncomp=6)
+hv6.bootmc = hvalidation(type="bootstrap", mc.runs=100, ncomp=6)
+hv6.kstn = hvalidation(type="none", samples=ks.full, ncomp=6)
+hv6.osim = hvalidation(type="none", samples=os.samples, ncomp=6)
+
+plot(hv6.rand$RMSE~hv6.rand$sample, type="l", xlab="Samples in training set", ylab="RMSE", col=1,
+    ylim=c(min(min(hv6.rand$RMSE), min(hv6.boot$RMSE), min(hv6.kstn$RMSE), min(hv6.osim$RMSE)),
+        max(max(hv6.rand$RMSE), max(hv6.boot$RMSE), max(hv6.kstn$RMSE), max(hv6.osim$RMSE))))
+lines(hv6.randmc$RMSE~hv6.randmc$sample, col=2)
+lines(hv6.boot$RMSE~hv6.boot$sample, col=3)
+lines(hv6.bootmc$RMSE~hv6.bootmc$sample, col=4)
+lines(hv6.kstn$RMSE~hv6.kstn$sample, col=5)
+lines(hv6.osim$RMSE~hv6.osim$sample, col=6)
+
+# RPD
+plot(hv6.rand$RPD~hv6.rand$sample, type="l", xlab="Samples in training set", ylab="RPD", col=1,
+    ylim=c(min(min(hv6.rand$RPD), min(hv6.boot$RPD), min(hv6.kstn$RPD), min(hv6.osim$RPD)),
+        max(max(hv6.rand$RPD), max(hv6.boot$RPD), max(hv6.kstn$RPD), max(hv6.osim$RPD))))
+lines(hv6.randmc$RPD~hv6.randmc$sample, col=2)
+lines(hv6.boot$RPD~hv6.boot$sample, col=3)
+lines(hv6.bootmc$RPD~hv6.bootmc$sample, col=4)
+lines(hv6.kstn$RPD~hv6.kstn$sample, col=5)
+lines(hv6.osim$RPD~hv6.osim$sample, col=6)
+
+av6.rand = hvalidation(type="random", validation.set=AUSTRALIA, ncomp=6)
+av6.randmc = hvalidation(type="random", mc.runs=100, validation.set=AUSTRALIA, ncomp=6)
+av6.boot = hvalidation(type="bootstrap", validation.set=AUSTRALIA, ncomp=6)
+av6.bootmc = hvalidation(type="bootstrap", mc.runs=100, validation.set=AUSTRALIA, ncomp=6)
+# ks.full has already been calculated, no need to redo that
+av.kstn = hvalidation(type="none", samples=ks.full, validation.set=AUSTRALIA, ncomp=6)
+# No need to recalculate os.samples either
+av.osim = hvalidation(type="none", samples=os.samples, validation.set=AUSTRALIA, ncomp=6)
+
+plot(av.rand$RMSE~av.rand$sample, type="l", xlab="Samples in training set", ylab="RMSE", col=1,
+    ylim=c(min(min(av.rand$RMSE), min(av.boot$RMSE), min(av.kstn$RMSE), min(av.osim$RMSE)),
+        max(max(av.rand$RMSE), max(av.boot$RMSE), max(av.kstn$RMSE), max(av.osim$RMSE))))
+lines(av.randmc$RMSE~av.randmc$sample, col=2)
+lines(av.boot$RMSE~av.boot$sample, col=3)
+lines(av.bootmc$RMSE~av.bootmc$sample, col=4)
+lines(av.kstn$RMSE~av.kstn$sample, col=5)
+lines(av.osim$RMSE~av.osim$sample, col=6)
+
+plot(av.rand$RPD~hv.rand$sample, type="l", xlab="Samples in training set", ylab="RPD", col=1,
+    ylim=c(min(min(av.rand$RPD), min(av.boot$RPD), min(av.kstn$RPD), min(av.osim$RPD)),
+        max(max(av.rand$RPD), max(av.boot$RPD), max(av.kstn$RPD), max(av.osim$RPD))))
+lines(av.randmc$RPD~av.randmc$sample, col=2)
+lines(av.boot$RPD~av.boot$sample, col=3)
+lines(av.bootmc$RPD~av.bootmc$sample, col=4)
+lines(av.kstn$RPD~av.kstn$sample, col=5)
+lines(av.osim$RPD~av.osim$sample, col=6)
