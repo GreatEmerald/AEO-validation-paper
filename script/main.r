3ba3c7ed440e2fa7bdbe356540acb0f229119159
Dainius <pastas4@gmail.com>
2016-05-06 18:44:38 +0200

    Addition of GPL headers

--- script/main.r
+++ script/main.r
@@ -1,4 +1,19 @@
-# Main script for AOE: Multivariate Analysis paper
+# Main script for AEO: Multivariate Analysis paper
+# Copyright (C) 2016 Dainius Masiliunas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 install.packages("inspectr", repos="http://R-Forge.R-project.org")
 library(pls)
 library(inspectr)
